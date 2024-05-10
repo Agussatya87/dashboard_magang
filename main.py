9da5ad411d97a7d1f7b0a68584d6be1078dc3a13
satya <satyapratama373@gmail.com>
2024-05-10 12:12:25 +0800

    create sidebar

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import pymongo
 
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
-st.title('Dashboard Siswa')
+st.title('Dashboard Siswa :student:')
 
 data = pd.read_csv("combine_final.csv")
 
 st.subheader('Raw data')
 st.write(data.tail(50))
+
+st.sidebar.title("Masukan Data Siswa :books:")
+input_data = st.sidebar.text_input(label="Nama", placeholder="masukan nama")
+
+if input_data:
+    filtered_data = data[data['name'].str.contains(input_data, case=False, na=False)]
+    st.subheader('Filtered Data')
+    st.write(filtered_data)
